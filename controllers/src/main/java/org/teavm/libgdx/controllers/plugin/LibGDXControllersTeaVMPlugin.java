aeca0b1c3bef208e488eb1c4f8e349254a50c207
konsoletyper <konsoletyper@gmail.com>
2015-10-12 21:26:57 +0300

    Add plugin ordering

--- controllers/src/main/java/org/teavm/libgdx/controllers/plugin/LibGDXControllersTeaVMPlugin.java
+++ controllers/src/main/java/org/teavm/libgdx/controllers/plugin/LibGDXControllersTeaVMPlugin.java
@@ -1,8 +1,12 @@
 package org.teavm.libgdx.controllers.plugin;
 
+import org.teavm.jso.impl.JSOPlugin;
+import org.teavm.vm.spi.After;
+import org.teavm.vm.spi.Before;
 import org.teavm.vm.spi.TeaVMHost;
 import org.teavm.vm.spi.TeaVMPlugin;
 
+@Before(JSOPlugin.class)
 public class LibGDXControllersTeaVMPlugin implements TeaVMPlugin {
     @Override
     public void install(TeaVMHost host) {
